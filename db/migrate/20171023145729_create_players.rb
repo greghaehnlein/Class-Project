cbb34b01735548eba63987bd29650ddd24dd208c
Greg Haehnlein <greghaehnlein@gmail.com>
2017-10-23 11:12:08 -0400

    added scaffolds

---
+++ db/migrate/20171023145729_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :position
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
